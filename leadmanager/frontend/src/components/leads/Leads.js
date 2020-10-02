8d26328591403a3fd1984adc67ef9d7a237a3991
Lance Dang <lancedang@Lances-MacBook-Pro.local>
2020-10-01 21:36:02 -0700

    Finish addform, delete, and getting data from backend

--- leadmanager/frontend/src/components/leads/Leads.js
+++ leadmanager/frontend/src/components/leads/Leads.js
@@ -1,13 +1,57 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { getLeads, deleteLead } from "../../actions/leads";
 
 export class Leads extends Component {
-    render() {
-        return (
-            <div>
-                <h1>Add Leads</h1>
-            </div>
-        )
-    }
+  static propTypes = {
+    leads: PropTypes.array.isRequired,
+    getLeads: PropTypes.func.isRequired,
+    deleteLead: PropTypes.func.isRequired,
+  };
+
+  componentDidMount() {
+    this.props.getLeads();
+  }
+  render() {
+    return (
+      <div>
+        <h2>Leads</h2>
+        <table className="table table-striped">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Message</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.props.leads.map((lead) => (
+              <tr key={lead.id}>
+                <th>{lead.name}</th>
+                <th>{lead.email}</th>
+                <th>{lead.message}</th>
+                <th>
+                  <button
+                    onClick={() => this.props.deleteLead(lead.id)}
+                    className="btn btn-danger btn-sm"
+                  >
+                    Delete
+                  </button>
+                </th>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
 }
 
-export default Leads
+const mapStateToProps = (state) => ({
+  leads: state.leads.leads,
+});
+
+export default connect(mapStateToProps, { getLeads, deleteLead })(Leads);
